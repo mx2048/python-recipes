e2fb555c61374fbb4cc957b8a309b18b18d77901
Max Podolskii <max.d.podolskii@gmail.com>
2018-12-16 22:59:30 -0800

    Added examples; finalized the module.

--- lazily-computed-properties.py
+++ lazily-computed-properties.py
@@ -3,6 +3,16 @@ Define an attribute as a property that only gets computed on access once,
 then the attribute is cached and returned from the cache on every access
 including initializing additional instances of the parent class
 and of the subclasses.
+
+Note that in the example the attribute `a` is computed via the method.
+As a matter of good practice, the result of the __init__ method must be a fully
+initialized object. What that means is that the object must be ready for use;
+it should not require some other settings to be tweaked or methods
+to be executed before it can perform its function.
+
+Credits:
+D. M. Beazley and B. K. Jones, Python cookbook
+W. Badenhorst, Practical Python Design Patterns
 """
 
 
@@ -27,4 +37,38 @@ class LazyProperty:
 
 
 if __name__ == '__main__':
-    pass
+
+    class A:
+        def __init__(self):
+            self.a = self.slow_method
+            pass
+
+        @LazyProperty
+        def slow_method(self):
+            print('Slow method is performing...')
+            return 1
+
+
+    class B(A):
+        def __init__(self):
+            super().__init__()
+            self.b = 2
+
+
+    a = A()
+    b = B()
+
+    print(a.a)
+    print(b.a)
+    print(A().slow_method)
+    print(b.slow_method)
+
+    # Output:
+
+    # Slow method is performing...
+    # 1
+    # 1
+    # 1
+    # 1
+
+    # Notice the slow method is computed only once.
