157d4211732f895bc2188068c6f3513e770929df
Max Podolskii <max.d.podolskii@gmail.com>
2018-12-16 22:32:21 -0800

    Added a module __doc__.

---
+++ lazily-computed-properties.py
@@ -0,0 +1,5 @@
+"""
+Define an attribute that only gets computed on access once, then the attribute
+is cached and returned from the cache on every access including initialzing
+additional instances of the parent class and of the subclasses.
+"""
