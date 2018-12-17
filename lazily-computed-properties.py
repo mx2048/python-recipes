c98a58895f96fd4bac7d5bfa393276f63a8aa4e0
Max Podolskii <max.d.podolskii@gmail.com>
2018-12-16 22:47:26 -0800

    Added class `LazyProperty`.

--- lazily-computed-properties.py
+++ lazily-computed-properties.py
@@ -1,5 +1,30 @@
 """
-Define an attribute that only gets computed on access once, then the attribute
-is cached and returned from the cache on every access including initialzing
-additional instances of the parent class and of the subclasses.
+Define an attribute as a property that only gets computed on access once,
+then the attribute is cached and returned from the cache on every access
+including initializing additional instances of the parent class
+and of the subclasses.
 """
+
+
+class LazyProperty:
+    """
+    Class as a property decorator around methods computed only once on access.
+
+    Consequent instantiating of the underlying class with the decorated methods
+    or the subclasses inheriting from the underlying class return the computed
+    values from the underlying class `__dict__`.
+    """
+    def __init__(self, func):
+        self.func = func
+
+    def __get__(self, instance, cls):
+        if instance is None:
+            return self
+        else:
+            value = self.func(cls)
+            setattr(cls, self.func.__name__, value)
+            return value
+
+
+if __name__ == '__main__':
+    pass
