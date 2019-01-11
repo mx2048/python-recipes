c5ea1e6bfe2833897033c43f443db09d7215713c
Max Podolskii <max.d.podolskii@gmail.com>
2019-01-11 14:07:12 -0800

    Refactored code: improved naming, expanded docstring.

--- calling_methods_via_decorator.py
+++ calling_methods_via_decorator.py
@@ -2,7 +2,7 @@ from functools import wraps
 from typing import Collection, Set
 
 
-def apply_for(attribute: str, included: Collection[str]=None, excluded: Collection[str]=None):
+def call_for(attribute: str, included: Collection[str]=None, excluded: Collection[str]=None):
     """
     Decorator: Call a decorated method of a class having `attribute`
     only when the attribute's value is in the `included` list
@@ -12,11 +12,14 @@ def apply_for(attribute: str, included: Collection[str]=None, excluded: Collecti
     in the `excluded` collection, always call the decorated method.
 
     Usage:
-    @apply_for(attribute='fruit', included='Apple, Carrot')
+    @call_for(attribute='fruit', included='Apple, Carrot')
     def peel(self):
         pass
+
+    where `self.fruit` must be 'apple' or 'carrot'
+    for method `peel` to be called.
     """
-    def param_wrapper(func):
+    def decorate(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
 
@@ -28,9 +31,6 @@ def apply_for(attribute: str, included: Collection[str]=None, excluded: Collecti
                     param = param.replace(',', ' ').split()
                 param = {uniform(e) for e in param}
                 return param
-
-            # Do not rewrite the nonlocal parameters.
-            nonlocal attribute, included, excluded
 
             self, *_ = args
             attr_value = getattr(self, attribute)
@@ -47,7 +47,7 @@ def apply_for(attribute: str, included: Collection[str]=None, excluded: Collecti
 
             return func(*args, **kwargs)
         return wrapper
-    return param_wrapper
+    return decorate
 
 
 if __name__ == '__main__':
@@ -57,21 +57,32 @@ if __name__ == '__main__':
             self.language = language
             self.data = 'No kudos yet'
 
-        @apply_for(attribute='language', included='Python, C', excluded='Java')
+        @call_for(attribute='language', included='Python, C', excluded='Java')
         def add_kudos(self, value):
-            self.data = f'Kudos acquired: {value} '
+            self.data = f'Kudos acquired: {value}'
+
+        @call_for('language', included='Java')
+        def deduct_kudos(self):
+            self.data = 'Kudos deducted'
 
     for lang in ['python', 'c', 'java', 'Perl']:
         obj = ProgrammingLanguage(lang)
         obj.add_kudos('100')
+        obj.deduct_kudos()
         print(f'{obj.language.capitalize()} - {obj.data}')
 
     # Output:
     # Python - Kudos acquired: 100
     # C - Kudos acquired: 100
-    # Java - No kudos yet
+    # Java - Kudos deducted
     # Perl - No kudos yet
 
     # Note, method `add_kudos` was called only for included languages.
     # The `ProgrammingLanguage` has attribute `language` which name was
-    # passed in `@apply_for()` decorator.
+    # passed in `@call_for()` decorator.
+
+    # Caveats:
+    #
+    # Do not rewrite the parameters passed
+    # to the outermost function `call_for` inside inner functions.
+    # You will get UnboundLocalError.
